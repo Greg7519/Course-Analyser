56e09c35d7e8c8e8e1ee6815dc067c88b7f749b4
= <greg.angeloppulos@outlook.com>
2026-04-24 19:55:00 +0300

    Starting web scraping with harvard courses. Currently it returns information but doesnt take parameters

---
+++ scraper.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+def HarvardScraper(append, url):
+    if append==False:
+        # Fetch and parse the page
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        # Find the main content container
+        content = soup.find('div', class_='main-wrapper grid-container full')
+        if content:
+            for div in content.find_all("div", class_='views-row'):
+                # parser price, checking each div once
+                for priceDetails in div.find_all("div",class_='field field---extra-field-pll-extra-field-price field--name-extra-field-pll-extra-field-price field--type- field--label-visually_hidden'):
+                    print(priceDetails.text)
+                for courseHeader in div.find_all("div",class_='field field--name-title field--type-string field--label-hidden field__items'):
+                    print(courseHeader.text)
+                for courseDetails in div.find_all("div",class_='text-teaser field field--name-field-summary field--type-text field--label-hidden field__items'):
+                    print(courseDetails.text)
+                for duration in div.find_all("div",class_="field field---extra-field-pll-extra-field-duration field--name-extra-field-pll-extra-field-duration field--type- field--label-visually_hidden"):
+                    print(duration.text)
+
+        else:
+            print("No article content found.")
+
+HarvardScraper(False,url="https://pll.harvard.edu/catalog")
