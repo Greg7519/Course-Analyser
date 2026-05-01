d97f00618cf807a1f88a99a9b77a41a675ef1104
Nick <262085749+Nikolas-Kop@users.noreply.github.com>
2026-05-01 12:53:50 +0300

    Made changes to scraper.py. Made HarvardScraper take more pages. Added CourseraScrapper but will not make it now

--- gui.py
+++ gui.py
@@ -7,17 +7,17 @@ from scraper import *
 
 scraped_data_df = None
 current_source_index = 0
-sources = [
+sources_api = [
     {"name": "Harvard", "url": "https://pll.harvard.edu/catalog"},
     {"name": "Udemy", "url": "https://www.udemy.com/courses/free/"},
-    {"name": "Coursera", "url": "https://www.coursera.org/"}
+    {"name": "Coursera", "url": "https://www.coursera.org/courses?query=free"}
 ]
 
 
 def add_subjects():
     global scraped_data_df, current_source_index
 
-    source = sources[current_source_index]
+    source = sources_api[current_source_index]
 
     print(f"[{source['name']}] Status: Starting Data Collection...")
     result_df = HarvardScraper(False, source['url'])
@@ -26,6 +26,12 @@ def add_subjects():
         scraped_data_df = result_df
         print(f"[{source['name']}] Status: Success")
         messagebox.showinfo("Success", f"Συλλέχθηκαν {len(result_df)} μαθήματα από {source['name']}")
+
+        listbox.delete(0, tk.END)
+        listbox.pack()
+
+        for title in scraped_data_df["Title"]:
+            listbox.insert(END, title)
     else:
         print(f"[{source['name']}] Status: Failed")
         messagebox.showerror("Error", "Αποτυχία συλλογής δεδομένων.")
@@ -48,10 +54,13 @@ def export_data():
 root = tk.Tk()
 root.geometry("1280x720")
 root.title("Web scraper Python ΑΓΓΕΛΟΠΟΥΛΟΣ ΓΡΗΓΟΡΙΟΣ ΠΑΝΑΓΙΩΤΗΣ 1115514 ΚΟΠΙΤΣΑΣ ΝΙΚΟΛΑΣ 115515")
+
 frame1 = tk.Frame(root, width=500, height=500)
 paddingYVal=15
+
 addSubjectsBtn = tk.Button(frame1, text="Προσθήκη μαθημάτων", width=25, command=add_subjects)
 addSubjectsBtn.pack(pady=paddingYVal,side="top")
+
 addSubjectsBtn = tk.Button(frame1, text="Εμφάνιση metadata μαθημάτων", width=25)
 addSubjectsBtn.pack(pady=paddingYVal)
 # Dropdown options
@@ -71,7 +80,7 @@ exportsBtn= tk.Button(frame1, text="Εξαγωγή δεδομένων σε CSV",
 exportsBtn.pack(pady=paddingYVal)
 
 
-listbox = tk.Listbox(frame1)
+listbox = tk.Listbox(frame1, width=100, font=("Calibri", 11))
 listbox.pack(pady=paddingYVal)
 listbox.pack_forget()
 
--- scraper.py
+++ scraper.py
@@ -3,52 +3,123 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-
-
 def HarvardScraper(append, url):
     all_Harvard_courses = []
-    if append == False:
+    current_page = 0
+
+    while append == False:
         # Fetch and parse the page
-        res = requests.get(url)
+        if append == True: break;
+
+        res = requests.get(url + "?page=" + str(current_page))
         soup = BeautifulSoup(res.content, 'html.parser')
+
         # Find the main content container
         content = soup.find('div', class_='main-wrapper grid-container full')
+
         if content:
             for block in content.find_all("div", class_='views-row'):
-                index=block.find("div", class_="datalayer-values")
-                title=index.get("data-item-name")
-                price=index.get("data-course-price")
-                difficulty=index.get("data-course-difficulty")
-                subject_category=index.get("data-item-category")
-                provider=index.get("data-course-school")
+                index = block.find("div", class_="datalayer-values")
+                title = index.get("data-item-name")
+                price = index.get("data-course-price")
+                difficulty = index.get("data-course-difficulty")
+                subject_category = index.get("data-item-category")
+                provider = index.get("data-course-school")
 
-                course_length=index.get("data-course-length")
-                if course_length=="": course_length="Ν/Α"
-                if course_length.upper().find("WEEK")!=-1:
-                    course_length=course_length[:course_length.upper().find("WEEK")]
-                    course_length=str(7*int(course_length))
-                if course_length.upper().find("MONTH")!=-1:
-                    course_length=course_length[:course_length.upper().find("MONTH")]
-                    course_length=str(31*int(course_length))
+                course_length = index.get("data-course-length")
+                if course_length == "": course_length = "Ν/Α"
+                if course_length.upper().find("WEEK") != -1:
+                    course_length = course_length[:course_length.upper().find("WEEK")]
+                    course_length = str(7 * int(course_length))
+                if course_length.upper().find("MONTH") != -1:
+                    course_length = course_length[:course_length.upper().find("MONTH")]
+                    course_length = str(31 * int(course_length))
 
-                course_language=index.get("data-course-language")
-
+                course_language = index.get("data-course-language")
 
                 course = {
                     "Title": title,
-                    "Price":price,
-                    "Difficulty":difficulty,
-                    "Subject Category":subject_category,
-                    "Provider":provider,
-                    "Course Length (in Days)":course_length,
-                    "Course Language":course_language
+                    "Price": price,
+                    "Difficulty": difficulty,
+                    "Subject Category": subject_category,
+                    "Provider": provider,
+                    "Course Length (in Days)": course_length,
+                    "Course Language": course_language
                 }
                 all_Harvard_courses.append(course)
+            current_page += 1
+            if current_page == 10: append = True
         else:
             print("No article content found.")
+            current_page += 1
+            if current_page == 10: append = True
 
-        df = pd.DataFrame(all_Harvard_courses)
-        return df
+    df = pd.DataFrame(all_Harvard_courses)
+
+    df = normalize_data(
+        df)  # συναρτηση οπου θα ομαδοποιουμε τις ονομασιες, πχ difficult, level 3, advanced -> 'Δύσκολο'
+
+    return df
 
 
+def CourseraScraper(append, url):
+    all_Coursera_courses = []
+    current_page = 1
 
+    while append == False:
+        # Fetch and parse the page
+        if append == True: break;
+
+        res = requests.get(url + "&page=" + str(current_page) + "#search-results-frame")
+        soup = BeautifulSoup(res.content, 'html.parser')
+
+        # Find the main content container
+        content = soup.find('div', class_='main-wrapper grid-container full')
+
+        if content:
+            for block in content.find_all("div", class_='views-row'):
+                index = block.find("div", class_="datalayer-values")
+                title = index.get("data-item-name")
+                price = index.get("data-course-price")
+                difficulty = index.get("data-course-difficulty")
+                subject_category = index.get("data-item-category")
+                provider = index.get("data-course-school")
+
+                course_length = index.get("data-course-length")
+                if course_length == "": course_length = "Ν/Α"
+                if course_length.upper().find("WEEK") != -1:
+                    course_length = course_length[:course_length.upper().find("WEEK")]
+                    course_length = str(7 * int(course_length))
+                if course_length.upper().find("MONTH") != -1:
+                    course_length = course_length[:course_length.upper().find("MONTH")]
+                    course_length = str(31 * int(course_length))
+
+                course_language = index.get("data-course-language")
+
+                course = {
+                    "Title": title,
+                    "Price": price,
+                    "Difficulty": difficulty,
+                    "Subject Category": subject_category,
+                    "Provider": provider,
+                    "Course Length (in Days)": course_length,
+                    "Course Language": course_language
+                }
+                all_Coursera_courses.append(course)
+            current_page += 1
+            if current_page == 10: append = True
+        else:
+            print("No article content found.")
+            current_page += 1
+            if current_page == 10: append = True
+
+    df = pd.DataFrame(all_Coursera_courses)
+
+    df = normalize_data(
+        df)  # συναρτηση οπου θα ομαδοποιουμε τις ονομασιες, πχ difficult, level 3, advanced -> 'Δύσκολο'
+
+    return df
+
+
+def normalize_data(df):
+    return df
