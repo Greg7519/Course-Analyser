b34c0d2d7a2001bcda3ac7371069a3832b6ff336
= <greg.angeloppulos@outlook.com>
2026-05-13 13:41:54 +0300

    Added scrollable windows(although not beautiful)

--- readFunctions.py
+++ readFunctions.py
@@ -4,6 +4,8 @@ import pandas as pd
 from tkinter import *
 from numpy.f2py.auxfuncs import l_and
 
+from scrollableFrame import ScrollableFrame
+
 
 def readMetadata():
     try:
@@ -14,24 +16,27 @@ def readMetadata():
             return
         window = Tk("Metadata")
         window.title("Harvard course metadata")
+
         window.geometry("1280x720")
+        frame = ScrollableFrame(window)
         # adding all the other rows into the grid
         column_names= df.columns
         i=0
         for j, col in enumerate(column_names):
-            text = Text(window, width=25, height=1, bg="#9BC2E6")
+            text = Text(frame.scrollable_frame, width=25, height=1, bg="#9BC2E6")
             text.grid(row=i, column=j)
             text.insert(INSERT, col)
         for i in range(rows):
             for j in range(cols):
-                text = Text(window, width=25, height=3)
+                text = Text(frame.scrollable_frame, width=25, height=3)
                 text.grid(row=i + 1, column=j)
 
                 text.insert(INSERT, df.values[i][j])
-
+        frame.pack(fill='both')
+        window.configure(width=rows * 25)
         window.mainloop()
     except:
-        print("No file found")
+        print("Error occured")
         exit()
 
 
@@ -45,18 +50,19 @@ def readFromCSVWithFilters(maxCost,language,category,difficulty,subjectNames,dro
         window = Tk("Metadata")
         window.title("Harvard course metadata")
         window.geometry("1280x720")
+        frame = ScrollableFrame(window)
+
         # adding all the other rows into the grid
         column_names = df.columns
         i = 0
-        # removing previous element
         dropdownMenu["menu"].delete(0, "end")
         for j, col in enumerate(column_names):
-            text = Text(window, width=25, height=1, bg="#9BC2E6")
+            text = Text(frame.scrollable_frame, width=25, height=1, bg="#9BC2E6")
             text.grid(row=i, column=j)
             text.insert(INSERT, col)
         for i in range(rows):
             for j in range(cols):
-                text = Text(window, width=25, height=3)
+                text = Text(frame.scrollable_frame, width=25, height=3)
                 text.grid(row=i + 1, column=j)
                 diffComp = (df.values[i][2].lower() == difficulty.lower() or difficulty == '')
                 categoryComp = (df.values[i][3].lower() == category.lower() or category == '')
@@ -71,6 +77,8 @@ def readFromCSVWithFilters(maxCost,language,category,difficulty,subjectNames,dro
                             label=df.values[i][j]
                         )
 
+        frame.pack(fill='both')
+        window.configure(width=rows*25)
         window.mainloop()
     except:
         print('Error occured!')
